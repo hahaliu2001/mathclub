8030acf8d174a803e9bdf07ce530b2f5e91c7eb4
hahaliu2001 <hahaliu2001@gmail.com>
2018-03-14 11:00:13 -0400

    1

---
+++ include/Matrix.h
@@ -0,0 +1,28 @@
+﻿#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <complex>
+
+namespace math_club
+{
+  class Matrix 
+  {
+  public:
+    typedef std::complex<double> Complex;
+    Matrix(void)  //无输入时，返回空函数
+    : rowSize(0), colSize(0) {};
+
+    Matrix(int rowLen, int colLen); //
+
+    Matrix（
+    
+  private:
+    int rowSize; //矩阵行数
+    int colSize; //矩阵列数
+    Complex *ArrayData; //矩阵数据
+
+  };
+}
+
+#endif
+
